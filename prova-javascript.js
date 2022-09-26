b4d9d178a36f997aefd36f1f8a9af552232a34f4
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2022-09-26 13:24:35 -0300

    PROVA JAVASCRIPT

---
+++ prova-javascript.js
@@ -0,0 +1,13 @@
+var bim1 = 6;
+var bim2 = 8;
+var bim3 = 3;
+var bim4 = 7;
+
+
+var mediaBimestral = (bim1+bim2+bim3+bim4)/4
+
+if(mediaBimestral >= 7){
+    console.log("APROVADO")
+}else{
+    console.log("REPROVADO")
+}
